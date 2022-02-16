012afe22d3845661bdab71eb47295882028d2b0e
Alexandre F <alexandreffaria@me.com>
2022-02-15 23:27:10 -0300

    regex stuff

---
+++ regex.py
@@ -0,0 +1,31 @@
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0,3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False
+    
+    if text[7] != '-':
+        return False
+    
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False
+
+    return True
+
+
+message = 'meu telefone é 382-428-4390, e meu celular é 321-320-4992'
+
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print(f'Achei um telefone: {chunk}')
+
+print('terminei de procurar')
